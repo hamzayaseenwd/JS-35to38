c0b331cf592b6c9abe9d4e7d5c3482ec6742b471
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-10-06 15:53:42 +0500

    Update

---
+++ app.js
@@ -0,0 +1,51 @@
+// Q1
+
+var today = new Date();
+
+document.write(today, )
+
+// Q2
+
+function greet() {
+  var firstname = prompt("Enter first name");
+  var lastname = prompt("Enter last name");
+  var fullname = firstname + lastname;
+  console.log(fullname);
+}
+
+greet();
+
+// Q3
+
+var num1 = prompt("Enter first Number to add");
+var num2 = prompt("Enter Second Number to add");
+
+var sum = num1 + num2;
+
+console.log(sum);
+
+// Q4
+
+var number1 = prompt("Enter first value");
+var oper = prompt("Enter operator value");
+var number2 = prompt("Enter Second value");
+
+function calc(number1, oper, number2) {
+  if (oper === "+") {
+    return number1 + number2;
+  } else if (oper === "-") {
+    return number1 - number2;
+  } else if (oper === "*") {
+    return number1 * number2;
+  } else if (oper === "/") {
+    return number1 / number2;
+  }
+}
+
+var result = calc(number1, oper, number2);
+
+console.log(result);
+
+
+// Q5
+
