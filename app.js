6124ec82e51f54479532ed09eb2ccd91a9ad701c
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-10-06 16:25:23 +0500

    js

--- app.js
+++ app.js
@@ -1,51 +1,59 @@
 // Q1
 
-var today = new Date();
+// var today = new Date();
 
-document.write(today, )
+// document.write(today, )
 
 // Q2
 
-function greet() {
-  var firstname = prompt("Enter first name");
-  var lastname = prompt("Enter last name");
-  var fullname = firstname + lastname;
-  console.log(fullname);
-}
+// function greet() {
+//   var firstname = prompt("Enter first name");
+//   var lastname = prompt("Enter last name");
+//   var fullname = firstname + lastname;
+//   console.log(fullname);
+// }
 
-greet();
+// greet();
 
 // Q3
 
-var num1 = prompt("Enter first Number to add");
-var num2 = prompt("Enter Second Number to add");
+// var num1 = prompt("Enter first Number to add");
+// var num2 = prompt("Enter Second Number to add");
 
-var sum = num1 + num2;
+// var sum = num1 + num2;
 
-console.log(sum);
+// console.log(sum);
 
 // Q4
 
-var number1 = prompt("Enter first value");
-var oper = prompt("Enter operator value");
-var number2 = prompt("Enter Second value");
+// var number1 = prompt("Enter first value");
+// var oper = prompt("Enter operator value");
+// var number2 = prompt("Enter Second value");
 
-function calc(number1, oper, number2) {
-  if (oper === "+") {
-    return number1 + number2;
-  } else if (oper === "-") {
-    return number1 - number2;
-  } else if (oper === "*") {
-    return number1 * number2;
-  } else if (oper === "/") {
-    return number1 / number2;
-  }
-}
+// function calc(number1, oper, number2) {
+//   if (oper === "+") {
+//     return number1 + number2;
+//   } else if (oper === "-") {
+//     return number1 - number2;
+//   } else if (oper === "*") {
+//     return number1 * number2;
+//   } else if (oper === "/") {
+//     return number1 / number2;
+//   }
+// }
 
-var result = calc(number1, oper, number2);
+// var result = calc(number1, oper, number2);
 
-console.log(result);
+// console.log(result);
 
 
 // Q5
+
+var number = prompt("Enter Number");
+
+var squarenumber = number * number
+
+console.log(squarenumber)
+
+// Q6
 
